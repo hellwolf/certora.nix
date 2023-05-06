40f53c4cdab334629604d444ef286585418c79da
Miao, ZhiCheng <miao@superfluid.finance>
2023-05-06 12:34:27 +0300

    update to certora-cli 3.6.8

--- flake.nix
+++ flake.nix
@@ -6,8 +6,8 @@
   '';
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
@@ -19,19 +19,25 @@
     certora-cli = p: with p; [(
       buildPythonPackage rec {
         pname = "certora-cli";
-        version = "3.6.5";
+        version = "3.6.8";
         src = fetchPypi {
           inherit pname version;
-          sha256 = "sha256-gN1m/W4kPzaXp4je2DMmQmfOF9xe1J0YK1pR10BIkDI=";
+          sha256 = "sha256-1j3KNxvY3+bZzxAELGoLtSyL8HFvrO8gMYq1pjkZWOU=";
         };
-        doCheck = false;
+        pythonRelaxDeps = true;
         propagatedBuildInputs = with pkgs.python3Packages; [
+          setuptools
           tabulate
           argcomplete
-          tqdm 
+          tqdm
           click
           pycryptodome
           sly
+          requests
+        ];
+        doCheck = false;
+        pythonImportsCheck = [
+          "certora_cli"
         ];
     })];
     devInputs = with pkgs; [
